a8757d3d7c400c82a4ca4a5282e6d7c67d712b96
WMBYBE <williambbrouillette@gmail.com>
2024-10-06 16:23:26 -0500

    q12

---
+++ Ch04Ex1MovieList/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Ch04Ex1MovieList.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ch04Ex1MovieList
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MovieContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("MovieContext")));
+        }
+    }
+}
